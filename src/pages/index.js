3d86fac58f1830e69792870652f664b999a72962
Erik Jonsberg <erikj@hungryghostdesign.com>
2023-02-10 10:08:54 -0500

    final cleanup and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,99 +1,48 @@
 import * as React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-// const links = [
-//   {
-//     text: "Tutorial",
-//     url: "https://www.gatsbyjs.com/docs/tutorial",
-//     description:
-//       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-//   },
-//   {
-//     text: "Examples",
-//     url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-//     description:
-//       "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-//   },
-//   {
-//     text: "Plugin Library",
-//     url: "https://www.gatsbyjs.com/plugins",
-//     description:
-//       "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-//   },
-//   {
-//     text: "Build and Host",
-//     url: "https://www.gatsbyjs.com/cloud",
-//     description:
-//       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-//   },
-// ]
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  h2 {
+    margin-bottom: .25rem;
+  }
+`
 
-// const samplePageLinks = [
-//   {
-//     text: "Page 2",
-//     url: "page-2",
-//     badge: false,
-//     description:
-//       "A simple example of linking to another page within a Gatsby site",
-//   },
-//   {
-//     text: "Page 3",
-//     url: "page-3",
-//     badge: false,
-//     description:
-//       "A simple example of linking to yet another page within a Gatsby site",
-//   },
-//   { text: "TypeScript", url: "using-typescript" },
-//   { text: "Server Side Rendering", url: "using-ssr" },
-//   { text: "Deferred Static Generation", url: "using-dsg" },
-// ]
-
-// const moreLinks = [
-//   { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-//   {
-//     text: "Documentation",
-//     url: "https://gatsbyjs.com/docs/",
-//   },
-//   {
-//     text: "Starters",
-//     url: "https://gatsbyjs.com/starters/",
-//   },
-//   {
-//     text: "Showcase",
-//     url: "https://gatsbyjs.com/showcase/",
-//   },
-//   {
-//     text: "Contributing",
-//     url: "https://www.gatsbyjs.com/contributing/",
-//   },
-//   { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-// ]
-
-//const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
 const MainPage = ({ data }) => (
   <Layout>
-  <Seo title='Home'/>
+    <Seo title="Home" />
     <div className={styles.textCenter}>
       <StaticImage
         src="../images/green-river-duck.png"
-        loading="eager"
+        loading="lazy"
         width={100}
         quality={95}
         formats={["auto", "webp", "avif"]}
         alt="logo"
         style={{ marginBottom: `var(--space-3)` }}
       />
+
       <h1>
         Erik's <b>Blog!</b>
       </h1>
-      <h4>Number of Posts: {data.allMarkdownRemark.totalCount}</h4>
     </div>
+    <h4>Number of Posts: {data.allMarkdownRemark.totalCount}</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <BlogLink to={node.fields.slug}>
+          <h2>{node.frontmatter.title}</h2>
+          <span>Published on {node.frontmatter.date}</span>
+        </BlogLink>
+        <p style={{ marginTop: `var(--space-1)` }}>{node.excerpt}</p>
+      </div>
+    ))}
   </Layout>
 )
 
@@ -106,7 +55,7 @@ export const Head = () => <Seo title="Home" />
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
       totalCount
       edges {
         node {
@@ -115,6 +64,9 @@ export const query = graphql`
             description
             title
             date
+          }
+          fields {
+            slug
           }
           excerpt
         }
